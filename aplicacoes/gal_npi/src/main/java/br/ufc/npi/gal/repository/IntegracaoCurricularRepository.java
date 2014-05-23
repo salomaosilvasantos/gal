d35c87caddf3bb1a6f3d1ff9bcaa3004e178fd0f
ErickBhrener <erick-bhrener@hotmail.com>
2014-05-23 10:50:35 -0300

    Repository, Service e Controller de IntegracaoCurricular adicionados..

---
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/repository/IntegracaoCurricularRepository.java
@@ -0,0 +1,9 @@
+package br.ufc.npi.gal.repository;
+
+import java.util.List;
+
+import br.ufc.npi.gal.model.IntegracaoCurricular;
+
+public interface IntegracaoCurricularRepository extends GenericRepository<IntegracaoCurricular> {
+	public List<IntegracaoCurricular> listar();
+}
