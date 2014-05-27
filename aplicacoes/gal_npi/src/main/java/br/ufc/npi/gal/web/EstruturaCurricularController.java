3265a657212054345381cfd3089c849653bcfeb6
Brendo94 <brendocolaco@outlook.com>
2014-05-27 11:11:27 -0300

    modificação em curso

--- aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/EstruturaCurricularController.java
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/EstruturaCurricularController.java
@@ -2,6 +2,7 @@ package br.ufc.npi.gal.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.ufc.npi.gal.service.EstruturaCurricularService;
@@ -12,4 +13,10 @@ public class EstruturaCurricularController {
 
 	@Autowired	
 	private EstruturaCurricularService estruturaCurricularService;
+	
+	@RequestMapping(value = "/listar.htm")
+	public String listar(ModelMap modelMap) {
+		modelMap.addAttribute("estruturas", this.estruturaCurricularService.listar());
+		return "estrutura/listar";
+	} 
 }
