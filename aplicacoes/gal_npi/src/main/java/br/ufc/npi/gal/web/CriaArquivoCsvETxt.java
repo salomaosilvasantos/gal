992cb829224bbbf8ff59bd0cf4c68adbee5196e6
salomaosilvasantos <salomaosilvasantos@gmail.com>
2014-09-17 11:57:45 -0300

    Retirei alguns comentários

--- aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/CriaArquivoCsvETxt.java
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/CriaArquivoCsvETxt.java
@@ -6,53 +6,52 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+public class CriaArquivoCsvETxt {
 
-public class CriaArquivoCsvETxt {	
-	
-	//Para a execução das duas classes abaixo é importar algumas classes do
-	//Java.
+	// Para a execução das duas classes abaixo é importar algumas classes do
+	// Java.
 	private File file;
-	
-	
+
 	public File getFile() {
 		return file;
 	}
+
 	public void setFile(File file) {
 		this.file = file;
 	}
-	
-	
-	public BufferedWriter abreFile(String nomeArquivo){
-			//Criação de um buffer para a escrita em uma stream
-			BufferedWriter StrW = null;
-			try {
-				file = new File(nomeArquivo);
-				StrW = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "Unicode"));
-							
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return StrW;
-		
+
+	public BufferedWriter abreFile(String nomeArquivo) {
+		// Criação de um buffer para a escrita em uma stream
+		BufferedWriter StrW = null;
+		try {
+			file = new File(nomeArquivo);
+			StrW = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(file), "Unicode"));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return StrW;
+
 	}
-	public void fechaFile(BufferedWriter StrW){
-		//Fechamos o buffer
+
+	public void fechaFile(BufferedWriter StrW) {
+		// Fechamos o buffer
 		try {
 			StrW.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+		
 			e.printStackTrace();
 		}
-	
+
 	}
-	
-	public void escreveFile(BufferedWriter StrW, String linha){
-		//Escrita dos dados da tabela
+
+	public void escreveFile(BufferedWriter StrW, String linha) {
+		// Escrita dos dados da tabela
 		try {
-			StrW.write(linha +"\n");
+			StrW.write(linha + "\n");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
 	}
