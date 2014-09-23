9aa00b1432fe61b0e9182958fcf59c1648027b14
francieudo <francieudo@npi-OptiPlex-7010>
2014-09-23 11:26:40 -0300

    Bug editar para curso corrigido.

--- aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/CursoController.java
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/CursoController.java
@@ -45,6 +45,10 @@ public class CursoController {
 		if (result.hasErrors()) {
 			return "curso/editar";
 		}
+		
+		if (result.hasFieldErrors("codigo")) {
+			return "curso/editar";
+		}
 
 		if (cursoService
 				.getOutroCursoByCodigo(curso.getId(), curso.getCodigo()) != null) {
