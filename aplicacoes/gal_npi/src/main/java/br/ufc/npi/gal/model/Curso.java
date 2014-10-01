65213a5748766258cbeb1bcb72e98dee8617085d
francieudo <francieudo@npi-OptiPlex-7010>
2014-10-01 08:03:50 -0300

    Removido comentários desnecessários no model Curso e codigo desnecessário no CursoController

--- aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/model/Curso.java
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/model/Curso.java
@@ -10,8 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -37,7 +35,6 @@ public class Curso implements Serializable {
 	@NotNull(message = "Campo obrigatório")
 	@Column(name = "cod_c")
 	@NumberFormat(style = Style.NUMBER)
-	//@Pattern(regexp = "[0-9]+", message = "O campo Codigo não pode possuir caracteres.")
 	private Integer codigo;
 
 	@NotEmpty(message = "Campo obrigatório")
