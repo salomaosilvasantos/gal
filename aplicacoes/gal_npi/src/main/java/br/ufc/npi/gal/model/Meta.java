31796db02793b433c43cca25ab79add864155a78
salomaosilvasantos <salomaosilvasantos@gmail.com>
2014-10-07 09:07:23 -0300

    Adicionei a classe Meta e uma página para configurar meta.

---
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/model/Meta.java
@@ -0,0 +1,100 @@
+package br.ufc.npi.gal.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "meta")
+public class Meta implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id_mt")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotEmpty(message = "Campo obrigatório")
+	@Column(name = "nome_c")
+	@Pattern(regexp = "[a-zA-Z\\sà-ùÀ-Ù]{0,}", message = "O campo Nome não pode possuir caracteres especiais ou números.")
+	private String nome;
+
+	private double indiceCalculoBasica;
+
+	private double indiceCalculoComplementar;
+
+	private boolean status;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getIndiceCalculoBasica() {
+		return indiceCalculoBasica;
+	}
+
+	public void setIndiceCalculoBasica(double indiceCalculoBasica) {
+		this.indiceCalculoBasica = indiceCalculoBasica;
+	}
+
+	public double getIndiceCalculoComplementar() {
+		return indiceCalculoComplementar;
+	}
+
+	public void setIndiceCalculoComplementar(double indiceCalculoComplementar) {
+		this.indiceCalculoComplementar = indiceCalculoComplementar;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Meta) {
+			Meta other = (Meta) obj;
+			if (other != null && other.getId() != null && this.id != null
+					&& other.getId().equals(this.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
