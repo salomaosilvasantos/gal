1ccb14e7e122a10323e5c6e7c90ff3b0204844a6
francieudo <francieudo@npi-OptiPlex-7010>
2014-11-06 08:14:39 -0300

    Alteração de variavel

--- aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/MetaController.java
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/web/MetaController.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
