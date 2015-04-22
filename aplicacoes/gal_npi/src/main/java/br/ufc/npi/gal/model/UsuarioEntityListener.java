5d80d1581a740012c58f9e10c40b9b77424ca824
s-holmes <rayson-santos@gmail.com>
2015-04-22 17:24:21 -0300

    Modificações no modelo, exclusão da entidade Papel

---
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/model/UsuarioEntityListener.java
@@ -0,0 +1,28 @@
+package br.ufc.npi.gal.model;
+
+import javax.persistence.PostLoad;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import br.ufc.quixada.npi.ldap.model.Usuario;
+import br.ufc.quixada.npi.ldap.service.UsuarioService;
+
+public class UsuarioEntityListener {
+	
+	@PostLoad
+	@Cacheable("loadProfessor")
+	public void loadUsuario(br.ufc.npi.gal.model.Usuario user) {
+		@SuppressWarnings("resource")
+		BeanFactory context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		UsuarioService usuarioService = (UsuarioService) context.getBean(UsuarioService.class);
+		Usuario usuario = usuarioService.getByCpf(user.getCpf());
+		
+		user.setEmail(usuario.getEmail());
+		user.setNome(usuario.getNome());
+		user.setSiape(usuario.getSiape());
+
+	}
+
+}
